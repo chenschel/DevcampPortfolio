bfd8f945415b5b1c17738058afac2b2b5ac8baf9
Christopher Henschel <christopher.henschel@sumcumo.com>
2018-03-29 23:01:35 +0200

    edit seed file image size

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,8 +21,8 @@ puts '5 Skills created'
         title: "My Portfolio #{x}",
         subtitle: 'Section 1.10.33 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC',
         body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        main_image: 'http://via.placeholder.com/600x400',
-        thumb_image: 'http://via.placeholder.com/350x200'
+        main_image: 'http://via.placeholder.com/300x150',
+        thumb_image: 'http://via.placeholder.com/120x60'
     )
 end
 
