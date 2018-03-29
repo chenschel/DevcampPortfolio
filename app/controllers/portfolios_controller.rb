d5fb43be9d9a8eba5b44f8f8ced4a0b73b08df7a
Christopher Henschel <christopher.henschel@sumcumo.com>
2018-03-29 22:01:02 +0200

    edit and update actions for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,7 +2,7 @@
 
 class PortfoliosController < ApplicationController
   def index
-    @portfolio_items = Portfolio.all
+    @portfolio_items = Portfolio.all.order(:id)
   end
 
   def new
@@ -20,6 +20,23 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+
+  def edit
+    @portfolio = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  private
 
   def portfolio_params
     params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image)
