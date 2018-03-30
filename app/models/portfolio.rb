70f52e687927a1bc7770ebedf917c561836d160b
Christopher Henschel <christopher.henschel@sumcumo.com>
2018-03-30 21:24:51 +0200

    implements validation for models

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class Portfolio < ApplicationRecord
+  validates :title, :subtitle, :body, :main_image, :thumb_image, presence: true
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class Skill < ApplicationRecord
+  validates :title, :percent_utilized, presence: true
 end
