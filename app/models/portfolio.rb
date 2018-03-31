64a2be23b12a47587a1cab1df876e8fccf91848b
Christopher Henschel <christopher.henschel@sumcumo.com>
2018-03-31 13:51:04 +0200

    defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,4 +4,11 @@ class Portfolio < ApplicationRecord
   validates :title, :subtitle, :body, :main_image, :thumb_image, presence: true
   scope :angular, -> { where(subtitle: 'Angular') }
   scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails') }
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= 'http://via.placeholder.com/300x150'
+    self.thumb_image ||= 'http://via.placeholder.com/120x60'
+  end
 end
