96f4c56b034341a6cb630e1d2cd41d52ead3baf3
Christopher Henschel <christopher.henschel@sumcumo.com>
2018-03-31 21:50:46 +0200

    integrates nested attributes for technologies and portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,8 +2,12 @@
 
 class Portfolio < ApplicationRecord
   has_many :technologies
+  accepts_nested_attributes_for :technologies, reject_if: ->(attrs) { attrs[:name].blank? }
+
   include Placeholder
+
   validates :title, :subtitle, :body, :main_image, :thumb_image, presence: true
+
   scope :angular, -> { where(subtitle: 'Angular') }
   scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails') }
 
