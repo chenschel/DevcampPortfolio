076ff5e9490985f1df82d9a05be69d86c81fcb5d
Christopher Henschel <christopher.henschel@sumcumo.com>
2018-03-31 20:51:58 +0200

    implements image generatorn as a concern

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates :title, :subtitle, :body, :main_image, :thumb_image, presence: true
   scope :angular, -> { where(subtitle: 'Angular') }
   scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails') }
@@ -8,7 +9,7 @@ class Portfolio < ApplicationRecord
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||= 'http://via.placeholder.com/300x150'
-    self.thumb_image ||= 'http://via.placeholder.com/120x60'
+    self.main_image ||= Placeholder.image_generator(height: 300, width: 150)
+    self.thumb_image ||= Placeholder.image_generator(height: 120, width: 60)
   end
 end
