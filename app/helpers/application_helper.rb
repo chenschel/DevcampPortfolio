cd534ccaef4c56d0f3f5dc971a61956da6d33765
Christopher Henschel <christopher.henschel@sumcumo.com>
2018-04-09 17:08:28 +0200

    implemented view helper for login links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def login_helper
+    if current_user.is_a?(User)
+      link_to('Logout', destroy_user_session_path, method: :delete)
+    else
+      login = link_to('Login', new_user_session_path)
+      register = link_to('Register', new_user_registration_path)
+      "#{login} #{register}".html_safe
+    end
+  end
 end
