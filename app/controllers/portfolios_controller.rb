10aa6e9ca932748bf406291bb6e03506068b189a
Christopher Henschel <christopher.henschel@sumcumo.com>
2018-04-12 12:37:23 +0200

    add before action for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class PortfoliosController < ApplicationController
+  before_action :portfolio, only: [:edit, :update, :show, :destroy]
   layout 'portfolio'
 
   def index
@@ -24,12 +25,9 @@ class PortfoliosController < ApplicationController
     end
   end
 
-  def edit
-    @portfolio = Portfolio.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @portfolio = Portfolio.find(params[:id])
     respond_to do |format|
       if @portfolio.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
@@ -39,12 +37,9 @@ class PortfoliosController < ApplicationController
     end
   end
 
-  def show
-    @portfolio = Portfolio.find(params[:id])
-  end
+  def show; end
 
   def destroy
-    @portfolio = Portfolio.find(params[:id])
     @portfolio.destroy
     respond_to do |format|
       format.html { redirect_to portfolios_path, notice: 'Record was removed.' }
@@ -56,5 +51,9 @@ class PortfoliosController < ApplicationController
   def portfolio_params
     params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image,
                                       technologies_attributes: [:name])
+  end
+
+  def portfolio
+    @portfolio = Portfolio.find(params[:id])
   end
 end
