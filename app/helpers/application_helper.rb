924e074a08eff9231f7c35139364c9454e03cebf
Christopher Henschel <christopher.henschel@sumcumo.com>
2018-04-15 22:23:35 +0200

    add gem redcarpet for markdown support on rendering Blog posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,7 +20,32 @@ module ApplicationHelper
 
   # rubocop:enable GuardClause
 
+  def markdown(text)
+    renderer = Redcarpet::Render::HTML.new(markdown_options)
+    markdown = Redcarpet::Markdown.new(renderer, markdown_extensions)
+
+    markdown.render(text).html_safe
+  end
+
   private
+
+  def markdown_extensions
+    {
+      autolink: true,
+      superscript: true,
+      disable_indented_code_blocks: true
+    }
+  end
+
+  def markdown_options
+    {
+      filter_html: true,
+      hard_wrap: true,
+      link_attributes: { rel: 'nofollow', target: '_blank' },
+      space_after_headers: true,
+      fenced_code_blocks: true
+    }
+  end
 
   def logout_link(style)
     link_to(
