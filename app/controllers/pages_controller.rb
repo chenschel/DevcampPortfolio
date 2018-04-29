fe799fa72827ebac2cfa1dbc2613ae1e4596e3b1
Christopher Henschel <christopher.henschel@sumcumo.com>
2018-04-29 20:11:06 +0200

    rm unnessacary page controller actions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
-  def home
-    @posts = Blog.all
-    @skills = Skill.all
-  end
-
   def social_media
     @tweets = SocialMedia::Twitter.search('#rubyonrails')
     @tweets += SocialMedia::Twitter.search('sumcumo')
