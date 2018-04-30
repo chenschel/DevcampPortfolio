71df0aa2ef8e202d8926bfdb7f78b09c98962f64
Christopher Henschel <christopher.henschel@sumcumo.com>
2018-04-30 21:37:08 +0200

    update comment styling

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module CommentsHelper
+  def avatar_helper(comment)
+    var_part = "#{comment.blog.friendly_id}#{comment.id}#{comment.blog.id}#{comment.user.id}"
+    url = "https://api.adorable.io/avatars/50/#{var_part}.png"
+    image_tag url, class: 'img-fluid'
+  end
 end
