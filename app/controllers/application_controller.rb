8fc451abffe32ecdb96f5d7e59b17653909c37b3
Christopher Henschel <christopher.henschel@sumcumo.com>
2018-05-18 21:18:39 +0200

    redirect to to current page after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,16 @@ class ApplicationController < ActionController::Base
   include CurrentUserConcern
   include DefaultPageContentConcern
   include MyViewToolConcern
+
+  before_action :store_user_location!, if: :storable_location?
+
+  private
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
+  end
 end
